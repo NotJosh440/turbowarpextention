650c98b3158d77b26876d344716d28c8ec21ee39
Joshua Angona <joshuaangona@icloud.com>
2023-07-04 10:31:50 -0400

    maybe

--- Extention.js
+++ Extention.js
@@ -1,81 +1,49 @@
 class HelloWorld {
   getInfo() {
     return {
-      id: 'Extention',
-      name: 'Extention',
+      id: 'Extension',
+      name: 'Extension',
       blocks: [
         {
-          opcode: 'negitive',
+          opcode: 'negative',
           blockType: Scratch.BlockType.REPORTER,
-          text: '-[NUMBER]',
+          text: '- [NUMBER]',
           arguments: {
             NUMBER: {
               type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '10'
-            }
+              defaultValue: '10',
+            },
           },
         },
-        // {
-        //   opcode: "costumeNumberOfSprite",
-        //   blockType: Scratch.BlockType.REPORTER,
-        //   text: "costume number of [SPRITE]",
-        //   arguments: {
-        //     SPRITE: {
-        //       type: Scratch.ArgumentType.STRING,
-        //     }
-        //   }
-        // }
-      ]
+        {
+          opcode: 'costumeNumberOfSprite',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'costume number of [SPRITE]',
+          arguments: {
+            SPRITE: {
+              type: Scratch.ArgumentType.STRING,
+            },
+          },
+        },
+      ],
     };
   }
 
-  function (ext) {
-    // Block definition
-    ext.getCostumeInfo = function (spriteName) {
-      var sprite = Stage.sprite[spriteName];
-      if (!sprite) {
-        return null;
-      }
-  
-      var costume = sprite.costumes[sprite.currentCostumeIndex];
-      if (!costume) {
-        return null;
-      }
-  
-      var costumeInfo = {
-        name: costume.costumeName,
-        number: sprite.currentCostumeIndex + 1
-      };
-  
-      return costumeInfo;
-    };
-  
-    // Block metadata
-    var descriptor = {
-      blocks: [
-        ['r', 'costume name of %s', 'getCostumeInfo', 'Sprite1'],
-        ['r', 'costume number of %s', 'getCostumeInfo', 'Sprite1']
-      ]
-    };
-    // Block metadata
-    var descriptor = {
-      blocks: [
-        ['r', 'costume name of %s', 'getCostumeInfo', 'Sprite1'],
-        ['r', 'costume number of %s', 'getCostumeInfo', 'Sprite1']
-      ]
+  negative(args) {
+    return -args.NUMBER;
+  }
+
+  costumeNumberOfSprite(args) {
+    const spriteName = args.SPRITE;
+    const sprite = Scratch.stage.getSpriteByName(spriteName);
+
+    if (sprite) {
+      const currentCostumeIndex = sprite.currentCostume;
+      return currentCostumeIndex + 1;
     }
-  }
-  
-    // Register the extension
 
-  negitive(args) {
-    return args.NUMBER * -1;
+    return 0;
   }
-  // costumeNumberOfSprite(args){
-  //   let differentSprite = args.SPRITE;
-  //   let currentCostumeIndex = differentSprite.costumeNumber();
-  //   return currentCostumeIndex;
-  // }
 }
 
-Scratch.extensions.register(new HelloWorld(), descriptor, ext);
+Scratch.extensions.register(new HelloWorld());
