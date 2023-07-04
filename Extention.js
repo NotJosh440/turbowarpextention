f9ba2de91b171755ffadb84bfb5b37efd52cdcd9
Joshua Angona <joshuaangona@icloud.com>
2023-07-04 11:04:15 -0400

    REV

--- Extention.js
+++ Extention.js
@@ -1,59 +1,41 @@
 class HelloWorld {
   getInfo() {
     return {
-      id: 'Extension',
-      name: 'Extension',
+      id: 'Extention',
+      name: 'Extention',
       blocks: [
         {
-          opcode: 'negative',
+          opcode: 'negitive',
           blockType: Scratch.BlockType.REPORTER,
-          text: '- [NUMBER]',
+          text: '-[NUMBER]',
           arguments: {
             NUMBER: {
               type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '10',
-            },
+              defaultValue: '10'
+            }
           },
         },
         {
-          opcode: 'costumeNumberOfSprite',
+          opcode: "costumeNumberOfSprite",
           blockType: Scratch.BlockType.REPORTER,
-          text: 'costume number of [SPRITE]',
+          text: "costume number of [SPRITE]",
           arguments: {
             SPRITE: {
               type: Scratch.ArgumentType.STRING,
-            },
-          },
-        },
-      ],
+            }
+          }
+        }
+      ]
     };
   }
 
-  negative(args) {
-    return -args.NUMBER;
+  negitive(args) {
+    return args.NUMBER * -1;
   }
-
-  getSpriteByName(spriteName) {
-    const targets = this.runtime.targets;
-    for (let i = 0; i < targets.length; i++) {
-      const target = targets[i];
-      if (target.sprite && target.sprite.name === spriteName) {
-        return target.sprite;
-      }
-    }
-    return null;
-  }
-
-  costumeNumberOfSprite(args) {
-    const spriteName = args.SPRITE;
-    const sprite = this.getSpriteByName(spriteName);
-
-    if (sprite) {
-      const currentCostumeIndex = sprite.getCurrentCostumeIndex();
-      return currentCostumeIndex + 1;
-    }
-
-    return 0;
+  costumeNumberOfSprite(args){
+    let differentSprite = args.SPRITE;
+    let currentCostumeIndex = differentSprite.costumeNumber();
+    return currentCostumeIndex;
   }
 }
 
