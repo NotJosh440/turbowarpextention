3498016e6343f4357567830c1a63e777095cbec8
Joshua Angona <joshuaangona@icloud.com>
2023-07-05 15:26:04 -0400

    is between block

--- Extention.js
+++ Extention.js
@@ -24,6 +24,25 @@ class HelloWorld {
               type: Scratch.ArgumentType.STRING,
             }
           }
+        },
+        {
+          opcode: "isBetween",
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: "is [INPUT] between [LOW] and [HIGH]?",
+          arguments: {
+            INPUT: {
+              type: Scratch.ArgumentType.NUMBER,
+              default: "2"
+            },
+            LOW: {
+              type: Scratch.ArgumentType.NUMBER,
+              default: "1"
+            },
+            HIGH: {
+              type: Scratch.ArgumentType.NUMBER,
+              default: "3"
+            },
+          }
         }
       ]
     };
@@ -32,9 +51,27 @@ class HelloWorld {
   negitive(args) {
     return args.NUMBER * -1;
   }
+
   costumeNumberOfSprite(args){
     return args.SPRITE.costumeNumber;
   }
+
+  isBetween(args){
+    if(args.LOW <= args.HIGH){ //LOW is less than HIGH
+      if(args.LOW <= args.INPUT && args.INPUT <= args.HIGH){ //LOW is less than INPUT and INPUT is less than HIGH
+        return true;
+      } else {
+        return false;
+      }
+    } else { //HIGH is less than LOW
+      if(args.LOW >= args.INPUT && args.INPUT >= args.HIGH){ //LOW is greater than INPUT and INPUT is greater than HIGH
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  
 }
 
 Scratch.extensions.register(new HelloWorld());
