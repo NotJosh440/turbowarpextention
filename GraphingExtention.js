224d70cf56b89b953debe400b2cb38f9724946cb
Joshua Angona <joshuaangona@icloud.com>
2023-11-17 16:59:04 -0500

    no toFixed()

--- GraphingExtention.js
+++ GraphingExtention.js
@@ -128,6 +128,7 @@ class GraphingExtension {
     ComplexPow(args){
         const result =  Complex(args.RONE, args.IONE).pow(args.RTWO, args.ITWO);
         const formattedResult = `${result.re} + ${result.im}i`;
+        
         return formattedResult;
     }
 }
