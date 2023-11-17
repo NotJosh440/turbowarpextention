a774c400fc5d6363a78b09818f527ba36251b0d2
Joshua Angona <joshuaangona@icloud.com>
2023-11-17 15:48:35 -0500

    maybe complex

--- GraphingExtention.js
+++ GraphingExtention.js
@@ -1,6 +1,19 @@
-import Complex from "complex.js";
-
-class GraphingExtension {
+async function loadModule() {
+    try {
+      // Dynamically import the module
+      const Complex = await import('complex.js');
+      
+      // Use the imported module
+      Complex();
+    } catch (error) {
+      console.error('Error loading module:', error);
+    }
+  }
+  
+  // Call the function to load the module when needed
+  loadModule();
+  
+  class GraphingExtension {
     getInfo() {
         return {
         id: 'findamount',
@@ -74,12 +87,19 @@ class GraphingExtension {
     }
 
     FloatToFraction(args) {
-        const tolerance = .0000000001;
+        x = args.NUM;
+        const tolerance = 1.0E-9;
+        let sign = '';
+        if (x < 0) {
+            sign = '-';
+            x = Math.abs(x);
+        }
+    
         let h1 = 1;
         let h2 = 0;
         let k1 = 0;
         let k2 = 1;
-        let b = args.NUM;
+        let b = x;
         
         do {
             const a = Math.floor(b);
@@ -90,9 +110,9 @@ class GraphingExtension {
             k1 = a * k1 + k2;
             k2 = temp;
             b = 1 / (b - a);
-        } while (Math.abs(args.NUM - h1 / k1) > args.NUM * tolerance);
+        } while (Math.abs(x - h1 / k1) > x * tolerance);
     
-        return `${h1}/${k1}`;
+        return `${sign}${h1}/${k1}`;
     }
 
     ComplexPow(args){
